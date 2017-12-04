582361fefdd139edc734cbad870d7e2bb95599ea
Yago <yagomus@windowslive.com>
2017-12-04 14:12:49 +0300

    add newspapers

--- newspaper_api.py
+++ newspaper_api.py
@@ -3,11 +3,11 @@ from newspapers import *
 
 app = Flask(__name__)
 
-newspaperlist=["milliyet","sabah","takvim"]
+newspaperlist=["milliyet","sabah","takvim","fotomac","dailysabah","yeniasir","anadolu","turkiye","hurriyet","yenisafak","star","aksam","birgun"]
 
 @app.route('/newspapers')
 def summary():
-    return jsonify(newspaperlist)
+    return jsonify({"newspapers":newspaperlist})
 
 
 @app.route('/')
@@ -22,12 +22,62 @@ def milliyet():
 @app.route('/sabah')
 def sabah():
     date=request.args.get('date')
-    return getSabah(date)
+    page=request.args.get('page', default = '1')
+    return getSabah(date, page)
 
 @app.route('/takvim')
 def takvim():
     date=request.args.get('date')
-    return getTakvim(date)
+    page=request.args.get('page', default = '1')
+    return getTakvim(date, page)
+
+@app.route('/fotomac')
+def fotomac():
+    date=request.args.get('date')
+    page=request.args.get('page', default = '1')
+    return getFotoMac(date, page)
+
+@app.route('/dailySabah')
+def dailySabah():
+    date=request.args.get('date')
+    page=request.args.get('page', default = '1')
+    return getDailySabah(date, page)
+
+@app.route('/yeniAsir')
+def yeniAsir():
+    date=request.args.get('date')
+    page=request.args.get('page', default = '1')
+    return getYeniAsir(date, page)
+
+@app.route('/anadolu')
+def anadolu():
+    date=request.args.get('date')
+    return getAnadolu(date)
+
+@app.route('/turkiye')
+def turkiye():
+    return getNetgazete("0")
+
+@app.route('/yenisafak')
+def yenisafak():
+    return getNetgazete("2")
+
+@app.route('/hurriyet')
+def hurriyet():
+    return getNetgazete("4")
+
+@app.route('/star')
+def star():
+    return getNetgazete("5")
+
+@app.route('/aksam')
+def aksam():
+    return getNetgazete("6")
+
+@app.route('/birgun')
+def birgun():
+    return getNetgazete("7")
+
 
 
 if __name__ == '__main__':
--- newspapers.py
+++ newspapers.py
@@ -10,26 +10,63 @@ def getMilliyet(date):
     return img
 
 
-def getSabah(date):
+def getSabah(date, page):
     base="http://egazete.sabah.com.tr/eGazete/image/www_sabah_com_tr/"
 
     date_splited=date.split('.')
-    url = base + date_splited[2] + "/" + date_splited[1] + "/" + date_splited[0] + "/1"
+    url = base + date_splited[2] + "/" + date_splited[1] + "/" + date_splited[0] + "/" + page
 
     return url
 
-def getTakvim(date):
+def getTakvim(date, page):
     base="http://egazete.takvim.com.tr/eGazete/image/www_takvim_com_tr/"
 
     date_splited=date.split('.')
-    url = base + date_splited[2] + "/" + date_splited[1] + "/" + date_splited[0] + "/1"
+    url = base + date_splited[2] + "/" + date_splited[1] + "/" + date_splited[0] + "/" + page
 
     return url
 
-def getFotoMac(date):
+def getFotoMac(date, page):
     base="http://egazete.fotomac.com.tr/eGazete/image/www_fotomac_com_tr/"
 
     date_splited=date.split('.')
-    url = base + date_splited[2] + "/" + date_splited[1] + "/" + date_splited[0] + "/1"
+    url = base + date_splited[2] + "/" + date_splited[1] + "/" + date_splited[0] + "/" + page
 
     return url
+
+def getDailySabah(date, page):
+    base="https://www.dailysabah.com/daily-paper-image/"
+
+    date_splited=date.split('.')
+    url = base + date_splited[2] + "/" + date_splited[1] + "/" + date_splited[0] + "/" + page
+
+    return url
+
+def getYeniAsir(date, page):
+    base="http://egazete.yeniasir.com.tr/eGazete/image/www_yeniasir_com_tr/"
+
+    date_splited=date.split('.')
+    url = base + date_splited[2] + "/" + date_splited[1] + "/" + date_splited[0] + "/" + page
+
+    return url
+
+def getAnadolu(date):
+    base="http://www.anadolugazetesi.com/egazete/?"
+
+    date_split = date.split('.')
+    url = base + "gun=" + date_split[0] + "&ay=" + date_split[1] + "&sene=" + date_split[2]
+
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    img = soup.select_one("div.resim > img")['src']
+    return "http://www.anadolugazetesi.com/" + img
+
+
+
+def getNetgazete(id):
+    base="http://gazete.netgazete.com/gazeteler.php?id="
+
+    page = requests.get(base+id)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    img = soup.select_one(".gazeteBuyukResim > a > img")['src']
+    return img
